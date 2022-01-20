ffdfd17d36e885011be47eb3cccaebe55a17d0bc
FENAGH <94008520+FENAGH@users.noreply.github.com>
2022-01-20 16:03:57 -0600

    Update data.js

--- src/config/data.js
+++ src/config/data.js
@@ -46,7 +46,7 @@ export const ASOCIACIONES = [
     asociacion: "AGAA",
     nombre: "Asociacion de Ganaderos y Agricultores de Atlantida",
     presidente: "Juan Alberto Sauceda",
-    telefono: "9478-7981"
+    telefono: "N/A"
   },
   {
     id: "02",
