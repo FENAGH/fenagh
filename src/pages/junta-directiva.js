f122deffc3269c44f3cb421e2e82bfa8e78da88f
RecaudoFenagh <Recaudo.Fenagh@gmail.com>
2025-05-27 08:53:42 -0600

    Cambio de periodo Junta directiva

--- src/pages/junta-directiva.js
+++ src/pages/junta-directiva.js
@@ -25,7 +25,7 @@ const DirectivePage = () => {
       <Hero
         sources={data.file.childImageSharp.fluid}
         className="hero-default hero-alignBottom hero-alignLeft"
-        headline="Junta Directiva 2023-2025"
+        headline="Junta Directiva 2025-2027"
         section="Nuestra Gente"
       />
       <Constrain>
